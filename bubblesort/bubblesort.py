7af491aa6fe40b7013eed665dadde8c5a03de43c
Toufik Ammoura <toufikammoura@gmail.com>
2020-11-21 16:08:26 +0100

    bubblesort done

--- bubblesort/bubblesort.py
+++ bubblesort/bubblesort.py
@@ -9,10 +9,19 @@ def isSorted(arr):
             return False
     return True
 
-def bubble(arr):
-    el = arr[0]
-    for i in range(len(arr)-1):    
-        if el > arr[i+1] 
-            
+def bubble(arr, end):
+    index = 0 
+    while index <= end:
+        if arr[index] > arr[index+1]:
+            swap(index, index+1, arr)
+        index += 1 
 
 def bubbleSort(arr):
+    end = len(arr)-2
+    while not isSorted(arr):
+        bubble(arr, end)
+        end -= 1
+
+arr = [5,4,7,8,9,1,4,3,10,12]
+bubbleSort(arr)
+print(arr)
