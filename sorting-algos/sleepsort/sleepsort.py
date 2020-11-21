b0c0ecc805cca1c28a76dd171d9a3a9e88b6cd3c
Toufik Ammoura <toufikammoura@gmail.com>
2020-11-21 20:21:09 +0100

    verplaatst in een map

---
+++ sorting-algos/sleepsort/sleepsort.py
@@ -0,0 +1,13 @@
+import threading
+from time import sleep
+
+def sleepy(num):
+    sleep(num)
+    print(num)
+
+def sleepsort(arr):
+    threadlist = [None] * len(arr)
+    threading.Thread.__init__()
+
+arr = [5,4,7,8,9,1,4,3,10,12]
+sleepsort(arr)
