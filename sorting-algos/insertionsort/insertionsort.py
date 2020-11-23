4c8ae0fc601ef66b4cfb313e8329c763d1546272
Toufik Ammoura <toufikammoura@gmail.com>
2020-11-23 19:43:33 +0100

    quicksort bijna af, kleine bug nog aanwezig

--- sorting-algos/insertionsort/insertionsort.py
+++ sorting-algos/insertionsort/insertionsort.py
@@ -1,5 +1,23 @@
-def insertionsort(arr):
+def isSorted(arr):
+    for index in range(len(arr)-1):
+        if arr[index] > arr[index+1]:
+            return False
+    return True
 
+def insert(arr, index):
+    el = arr.pop(index)
+    i = 0
+    while i < index:
+        if el < arr[i]:
+            arr.insert(i, el)
+            break
+        i+=1
+
+def insertionsort(arr):
+    i = 1
+    while not isSorted(arr) and i < len(arr):
+        insert(arr, i)
+        i+=1
 
 arr = [5,4,7,8,9,1,4,3,10,12]
 insertionsort(arr)
