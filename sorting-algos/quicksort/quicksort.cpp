d29cf128d6ae434eaf2964aba9d9c8092b71e071
Toufik Ammoura <toufikammoura@gmail.com>
2020-11-23 14:03:26 +0100

    quicksort begin c++

---
+++ sorting-algos/quicksort/quicksort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void printArr(int arr[], int size){
+    int i=0;
+    while(i < size){
+        cout << arr[i++] << " ";
+    }
+    cout << "\n";
+}
+
+void quicksort(int* arr, int size){
+
+}
+
+int main(){
+    int arr[] = {4,6,2,8,9,86,12,34,1,0};
+    int arraySize = sizeof(arr)/sizeof(arr[0]);
+
+    quicksort(arr, arraySize);
+    printArr(arr, arraySize);
+    return 0;
+}
