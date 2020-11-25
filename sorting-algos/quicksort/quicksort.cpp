0b30eb5a895594d17a90c8f85e995d93734f8699
Toufik Ammoura <toufikammoura@gmail.com>
2020-11-25 19:04:09 +0100

    insert functie toegevoegd

--- sorting-algos/quicksort/quicksort.cpp
+++ sorting-algos/quicksort/quicksort.cpp
@@ -10,7 +10,25 @@ void printArr(int arr[], int size){
     cout << "\n";
 }
 
-void quicksort(int* arr, int size){
+void insert(int* arr, int index, int el, int size){
+    // we hebben denk ik een insert functie nodig (ookal kost dit performance)
+    int newArr[size+1];
+    int i=0;
+    while(i < index){
+        arr[i] = newArr[i]; // copying one array into the other
+        i++;
+    }
+    
+    newArr[i] = el; // i will be equal to index so we insert the element
+    
+    while(i < size){
+        newArr[i+1] = arr[i]; // newArr is now one element bigger so i+1 
+        i++;
+    }
+}
+
+void quicksort(int* arr, int begin, int end){
+
 
 }
 
@@ -18,7 +36,7 @@ int main(){
     int arr[] = {4,6,2,8,9,86,12,34,1,0};
     int arraySize = sizeof(arr)/sizeof(arr[0]);
 
-    quicksort(arr, arraySize);
+    quicksort();
     printArr(arr, arraySize);
     return 0;
 }
