0149920625aefab17c425fdd17c6fa9e8bf5213c
Toufik Ammoura <toufikammoura@gmail.com>
2020-12-05 23:10:39 +0100

    na gedacht over data structuren

--- graph-algos/dijkstra/dijkstra.py
+++ graph-algos/dijkstra/dijkstra.py
@@ -1,5 +1,11 @@
-# Notes:
+from math import inf
+
+# NOTES:
 # Dijkstra berekent de kortste route van een bepaald punt naar alle punten
+# We moeten een class of struct maken voor iedere node want we moeten best wat dingen opslaan
+#   - Totale afstand vanaf start
+#   - Route van start naar de node
+# Dit kan misschien in een dictionary met tuples (distance, "a->c->h->this")
 
 # BEGIN:
 # We verdelen de punten in drie groepen:
@@ -16,8 +22,27 @@
 # Neem de kortste route die er is, als je een node voor de eerste keer ontdekt dan zal dat de kortste route zijn om daar te komen
 # x verplaats je nu van X naar A omdat die bepaald is, er zal geen snellere route zijn naar die node 
 
+routes = {}
+
+graph = {}
+
+adjacencyMatrix = [[0,4,4,6,6,0,0,0,2],
+                   [4,0,2,0,0,0,0,0,0],
+                   [4,2,0,8,0,0,1,0,0],
+                   [6,0,8,0,9,2,0,1,0],
+                   [6,0,0,9,0,0,0,2,2],
+                   [0,0,0,2,0,0,2,1,0],
+                   [0,0,1,0,0,2,0,0,0],
+                   [0,0,0,1,2,1,0,0,3],
+                   [2,0,0,0,2,0,0,3,0] ]
+
 A = set()
 X = set()
 
-def init(begin):
-    A.add(begin)
+def init(start, graphSize):
+    for i in range(graphSize):
+        graph[i] = (inf, "")
+    A.add(start)
+
+def dijkstra(start, destination, matrix):
+    raise NotImplementedError()
